e32aff918ccb55c395b1fa4f1984a5a3f9111943
Walliston <tontongamer111@gmail.com>
2024-11-11 15:56:21 -0300

    segunda versão

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
+import { useState } from "react"
+import "./Style.scss"
 export default function App() {
+  //variavel de estado
+  const [primeiroValor, setSegundoValor] =useState()
   return (
     <main>
       <h1>Calculadora</h1>
       <input type="number" placeholder="Insira um número" />
       <input type="number" placeholder="Insira um número" />
       <div>
-      <button>+</button>
-      <button>-</button>
-      <button>/</button>
-      <button>x</button>
-    </div>
+        <button>+</button>
+        <button>-</button>
+        <button>/</button>
+        <button>x</button>
+      </div>
     </main>
   )
 }
