c8b000f7479808d7b23359e4eecafb9da8a4baee
Walliston <tontongamer111@gmail.com>
2024-11-13 15:53:11 -0300

    logica

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,27 @@
 import { useState } from "react"
+// hook do react para mudanças do estado
 import "./Style.scss"
 export default function App() {
-  //variavel de estado
-  const [primeiroValor, setSegundoValor] =useState()
+  // variavel de estado
+  // primeiro é a variavel de estado
+  // setPrimeiroValor é a função atulizadora
+  const [primeiroValor, setPrimeiroValor] =useState();
+  const [segundoValor, setSegundoValor] =useState();
+
+  // arrow function () => {}
+    // função especial
+  const capturandoPrimeiroValor = (e) => {
+    setPrimeiroValor(Number(e.target.value))
+  }
+
+  const capturandoSegundoValor = (e) => {
+    setSegundoValor(Number(e.target.value))
+  }
   return (
     <main>
       <h1>Calculadora</h1>
-      <input type="number" placeholder="Insira um número" />
-      <input type="number" placeholder="Insira um número" />
+      <input onChange={capturandoPrimeiroValor} type="number" placeholder="Insira um número" />
+      <input onChange={capturandoSegundoValor} type="number" placeholder="Insira um número" />
       <div>
         <button>+</button>
         <button>-</button>
